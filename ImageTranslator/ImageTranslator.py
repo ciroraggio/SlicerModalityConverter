536f9a9830e401a391c4a11ea7b1bfd05c80d52a
Ciro Raggio <ciroraggio25@gmail.com>
2025-08-01 17:25:51 +0200

    Updated module category

--- ImageTranslator/ImageTranslator.py
+++ ImageTranslator/ImageTranslator.py
@@ -29,7 +29,7 @@ class ImageTranslator(ScriptedLoadableModule):
         ScriptedLoadableModule.__init__(self, parent)
 
         self.parent.title = _("ImageTranslator")
-        self.parent.categories = [translate("qSlicerAbstractCoreModule", "Quantification")]
+        self.parent.categories = [translate("qSlicerAbstractCoreModule", "I2IHub")]
         self.parent.dependencies = []
         self.parent.contributors = CONTRIBUTORS
         self.parent.helpText = _(HELP_TEXT)
